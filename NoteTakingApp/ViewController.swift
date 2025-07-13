c6e4f373503bf6b75a3b950873156988c04f8a46
zulalsarioglu <zulal.sariogluu@gmail.com>
2025-07-13 16:37:34 +0300

    Initial Commit

---
+++ NoteTakingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteTakingApp
+//
+//  Created by Zülal Sarıoğlu on 13.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
